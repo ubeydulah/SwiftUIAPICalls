81ddc3fdec880ba8d87a02cc18e6122d3e46d546
Ubeydullah <109802244+ubeydulah@users.noreply.github.com>
2022-10-24 19:52:22 +0300

    Add files via upload

---
+++ SwiftUIAPICalls/SwiftUIAPICalls/ContentView.swift
@@ -0,0 +1,82 @@
+//
+//  ContentView.swift
+//  SwiftUIAPICalls
+//
+//  Created by ubeyt on 18.09.2022.
+//
+
+import SwiftUI
+
+struct URLImage: View {
+    
+    let urlString: String
+    
+    @State var data: Data?
+    
+    var body: some View{
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+        }
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear{
+                  fetchData()
+            }
+        }
+    }
+    private func fetchData() {
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+}
+
+
+struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+    
+    
+    var body: some View {
+        
+        NavigationView{
+            List{
+                ForEach(viewModel.courses, id: \.self) { course in
+                    
+                    HStack{
+                        URLImage(urlString: course.image, data: nil)
+                        Text(course.name)
+                            .bold()
+                    }
+                    .padding(3)
+                    
+                    
+                }
+            }
+            .navigationTitle("Courses")
+            .onAppear{
+                viewModel.fetch()
+            }
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIAPICalls/SwiftUIAPICalls/SwiftUIAPICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPICallsApp.swift
+//  SwiftUIAPICalls
+//
+//  Created by ubeyt on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
